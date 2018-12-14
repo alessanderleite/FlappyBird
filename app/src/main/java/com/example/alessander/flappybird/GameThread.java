ae7ae5f9d014d1d45201bc9e28ccd91b4483476d
alessanderleite <alessander.logistica@gmail.com>
2018-12-14 07:41:32 -0200

    build a new class GameThread

---
+++ app/src/main/java/com/example/alessander/flappybird/GameThread.java
@@ -0,0 +1,46 @@
+package com.example.alessander.flappybird;
+
+import android.graphics.Canvas;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.SurfaceHolder;
+
+public class GameThread extends Thread {
+
+    SurfaceHolder surfaceHolder; //SurfaceHolder object reference
+    boolean isRunning; //Flag to detect whether the thread is running or not
+    long startTime, loopTime; //Loop start time and loop duration
+    long DELAY = 33; //Delay in milliseconds between screen refreshes
+
+    public GameThread(SurfaceHolder surfaceHolder) {
+        this.surfaceHolder = surfaceHolder;
+        isRunning = true;
+    }
+
+    @Override
+    public void run() {
+        // Looping until the boolean is false
+        while (isRunning) {
+            startTime = SystemClock.uptimeMillis();
+            //Locking the canvas
+            Canvas canvas = surfaceHolder.lockCanvas(null);
+            if (canvas != null) {
+                synchronized (surfaceHolder) {
+                    AppConstants.getGameEnginge().updateAndDrawBackgroundImage(canvas);
+                    //unlocking the canvas
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+            }
+            //loop time
+            loopTime = SystemClock.uptimeMillis() - startTime;
+            if (loopTime < DELAY) {
+                try {
+                    Thread.sleep(DELAY - loopTime);
+                } catch (InterruptedException e) {
+                    Log.e("Interrupted", "Interrupted while sleeping");
+                }
+
+            }
+        }
+    }
+}
