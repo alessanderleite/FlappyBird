c8ba1c1da00b2b2059140f6afb7d5b887dd9c91b
alessanderleite <alessander.logistica@gmail.com>
2018-12-15 15:42:50 -0200

    add scaleImage method inside BitmapBank class

--- app/src/main/java/com/example/alessander/flappybird/BitmapBank.java
+++ app/src/main/java/com/example/alessander/flappybird/BitmapBank.java
@@ -10,6 +10,7 @@ public class BitmapBank {
 
     public BitmapBank(Resources res) {
         background = BitmapFactory.decodeResource(res, R.drawable.background);
+        background = scaleImage(background);
     }
 
     //Return background bitmap
@@ -26,4 +27,15 @@ public class BitmapBank {
     public int getBackgroundHeight() {
         return background.getHeight();
     }
+
+    public Bitmap scaleImage(Bitmap bitmap) {
+        float widthHeightRatio = getBackgroundWidth() / getBackgroundHeight();
+        /*
+        * We'll multiply widthHeightRatio with screenHeight to get scaled width of the bitmap
+        * Then call createScaledBitmap() to create a new bitmap, scaled from an existing bitmap, when possible
+        * */
+        int backgroundScaledWidth = (int) widthHeightRatio * AppConstants.SCREE_HEIGHT;
+        return Bitmap.createScaledBitmap(bitmap, backgroundScaledWidth, AppConstants.SCREE_HEIGHT, false);
+    }
 }
+
