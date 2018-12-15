fbb2fcedee1c0a6c51236f47acf46fe7e72024d7
alessanderleite <alessander.logistica@gmail.com>
2018-12-15 17:07:18 -0200

    call Bird class and create the updateAndDrawBird method

--- app/src/main/java/com/example/alessander/flappybird/GameEngine.java
+++ app/src/main/java/com/example/alessander/flappybird/GameEngine.java
@@ -5,10 +5,11 @@ import android.graphics.Canvas;
 public class GameEngine {
 
     BackgroundImage backgroundImage;
+    Bird bird;
 
     public GameEngine() {
         backgroundImage = new BackgroundImage();
-
+        bird = new Bird();
     }
 
     public void updateAndDrawBackgroundImage(Canvas canvas) {
@@ -21,5 +22,16 @@ public class GameEngine {
         if (backgroundImage.getX() < -(AppConstants.getBitmapBank().getBackgroundWidth() - AppConstants.SCREEN_WIDTH)) {
             canvas.drawBitmap(AppConstants.getBitmapBank().getBackground(), backgroundImage.getX() + AppConstants.getBitmapBank().getBackgroundWidth(), backgroundImage.getY(), null);
         }
+    }
+
+    public void updateAndDrawBird(Canvas canvas) {
+        int currentFrame = bird.getCurrentFrame();
+        canvas.drawBitmap(AppConstants.getBitmapBank().getBird(currentFrame), bird.getX(), bird.getY(), null);
+        currentFrame++;
+        // If it exceeds maxframe re-initialize to 0;
+        if (currentFrame > bird.maxFrame) {
+            currentFrame = 0;
+        }
+        bird.setCurrentFrame(currentFrame);
     }
 }
