fac3e6b14663ee3d2739b9cd3c84542d2666966e
alessanderleite <alessander.logistica@gmail.com>
2018-12-15 14:43:47 -0200

    declare getBitmapBank and getGameEngine methods

--- app/src/main/java/com/example/alessander/flappybird/AppConstants.java
+++ app/src/main/java/com/example/alessander/flappybird/AppConstants.java
@@ -1,5 +1,23 @@
 package com.example.alessander.flappybird;
 
+import android.content.Context;
+
 public class AppConstants {
+
+    static BitmapBank bitmapBank; //Bitmap object reference
+    static GameEngine gameEngine; //GameEngine object reference
+
+    public static void initialization(Context context) {
+        bitmapBank = new BitmapBank(context.getResources());
+        gameEngine = new GameEngine();
+    }
+
+    public static BitmapBank getBitmapBank() {
+        return bitmapBank;
+    }
+
+    public static GameEngine getGameEngine() {
+        return gameEngine;
+    }
 
 }
