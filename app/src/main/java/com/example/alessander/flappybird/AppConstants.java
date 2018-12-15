904e90f57027fafa41dbea626f0b2392cf85a30e
alessanderleite <alessander.logistica@gmail.com>
2018-12-15 15:45:31 -0200

    set Screen Size method to ajust layout

--- app/src/main/java/com/example/alessander/flappybird/AppConstants.java
+++ app/src/main/java/com/example/alessander/flappybird/AppConstants.java
@@ -1,13 +1,18 @@
 package com.example.alessander.flappybird;
 
 import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
 
 public class AppConstants {
 
     static BitmapBank bitmapBank; //Bitmap object reference
     static GameEngine gameEngine; //GameEngine object reference
+    static int SCREEN_WIDTH, SCREE_HEIGHT;
 
     public static void initialization(Context context) {
+        setScreenSize(context);
         bitmapBank = new BitmapBank(context.getResources());
         gameEngine = new GameEngine();
     }
@@ -19,5 +24,18 @@ public class AppConstants {
     public static GameEngine getGameEngine() {
         return gameEngine;
     }
+
+    private static void setScreenSize(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+        AppConstants.SCREEN_WIDTH = width;
+        AppConstants.SCREE_HEIGHT = height;
+
+    }
+
 
 }
