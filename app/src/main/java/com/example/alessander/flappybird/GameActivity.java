7188aefb849643820550f90e7e1382523e3ce495
alessanderleite <alessander.logistica@gmail.com>
2018-12-15 15:44:15 -0200

    call GameView and add setContentView inside GameActivity class

--- app/src/main/java/com/example/alessander/flappybird/GameActivity.java
+++ app/src/main/java/com/example/alessander/flappybird/GameActivity.java
@@ -6,8 +6,12 @@ import android.support.annotation.Nullable;
 
 public class GameActivity extends Activity {
 
+    GameView gameView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        gameView = new GameView(this);
+        setContentView(gameView);
     }
 }
