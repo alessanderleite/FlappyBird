95cd8d7535734cc7dd5d8c228935ca7031536a20
alessanderleite <alessander.logistica@gmail.com>
2018-12-15 11:47:35 -0200

    set constructor, getters and setters inside BackgroundImage class

--- app/src/main/java/com/example/alessander/flappybird/BackgroundImage.java
+++ app/src/main/java/com/example/alessander/flappybird/BackgroundImage.java
@@ -1,4 +1,36 @@
 package com.example.alessander.flappybird;
 
 public class BackgroundImage {
+
+    private int backgroundImageX, backgroundImageY, backgroundImageVelocity;
+
+    public BackgroundImage() {
+        backgroundImageX = 0;
+        backgroundImageY = 0;
+        backgroundImageVelocity = 3;
+    }
+
+    public int getBackgroundImageX() {
+        return backgroundImageX;
+    }
+
+    public void setBackgroundImageX(int backgroundImageX) {
+        this.backgroundImageX = backgroundImageX;
+    }
+
+    public int getBackgroundImageY() {
+        return backgroundImageY;
+    }
+
+    public int getBackgroundImageVelocity() {
+        return backgroundImageVelocity;
+    }
+
+    public void setBackgroundImageVelocity(int backgroundImageVelocity) {
+        this.backgroundImageVelocity = backgroundImageVelocity;
+    }
+
+    public void setBackgroundImageY(int backgroundImageY) {
+        this.backgroundImageY = backgroundImageY;
+    }
 }
