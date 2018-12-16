d520b828605f21aec099040117327acefa146de8
alessanderleite <alessander.logistica@gmail.com>
2018-12-16 10:00:50 -0200

    create the method setGameConstants

--- app/src/main/java/com/example/alessander/flappybird/AppConstants.java
+++ app/src/main/java/com/example/alessander/flappybird/AppConstants.java
@@ -13,15 +13,31 @@ public class AppConstants {
     static int gravity;
     static int VELOCITY_WHEN_JUMPED;
     static int gapBetweenTopAndBottomTubes;
+    static int numberOfTubes;
+    static int tubeVelocity;
+    static int minTubeOffsetY;
+    static int maxTubeOffsetY;
+    static int distanceBetweenTubes;
 
     public static void initialization(Context context) {
         setScreenSize(context);
         bitmapBank = new BitmapBank(context.getResources());
+        setGameConstants();
         gameEngine = new GameEngine();
+
+    }
+
+    public static void setGameConstants() {
         AppConstants.gravity = 3;
         AppConstants.VELOCITY_WHEN_JUMPED = -40;
         gapBetweenTopAndBottomTubes = 600;
+        AppConstants.numberOfTubes = 2;
+        AppConstants.tubeVelocity = 12;
+        AppConstants.minTubeOffsetY = (int)(AppConstants.gapBetweenTopAndBottomTubes / 2.0);
+        AppConstants.maxTubeOffsetY = AppConstants.SCREE_HEIGHT - AppConstants.minTubeOffsetY - AppConstants.gapBetweenTopAndBottomTubes;
+        AppConstants.distanceBetweenTubes = AppConstants.SCREEN_WIDTH * 3 / 4;
     }
+
     // Return BitmapBank instance
     public static BitmapBank getBitmapBank() {
         return bitmapBank;
