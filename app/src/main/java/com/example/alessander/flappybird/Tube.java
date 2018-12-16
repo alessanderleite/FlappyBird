7b5344c1dcadee6d014953d79f02796d8189e89f
alessanderleite <alessander.logistica@gmail.com>
2018-12-15 22:43:06 -0200

    new class Tube with constract a gettters

---
+++ app/src/main/java/com/example/alessander/flappybird/Tube.java
@@ -0,0 +1,41 @@
+package com.example.alessander.flappybird;
+
+import java.util.Random;
+
+public class Tube {
+    // tubeX = Tube X-coordinate, topTubeOffsetY = top tube bottom edge coordinate
+    private int tubeX, topTubeOffsetY;
+    private Random random;
+    public Tube(int tubeX, int topTubeOffsetY) {
+        this.tubeX = tubeX;
+        this.topTubeOffsetY = topTubeOffsetY;
+        random = new Random();
+    }
+
+    public int getTopTubeOffsetY() {
+        return topTubeOffsetY;
+    }
+
+    public int getTubeX() {
+        return tubeX;
+    }
+
+    public int getTopTubeY() {
+        return topTubeOffsetY - AppConstants.getBitmapBank().getTubeHeight();
+    }
+
+    public int getBottomTubeY() {
+        return topTubeOffsetY + AppConstants.gapBetweenTopAndBottomTubes;
+    }
+
+    public void setTubeX(int tubeX) {
+        this.tubeX = tubeX;
+    }
+
+    public void setTopTubeOffsetY() {
+        this.topTubeOffsetY = topTubeOffsetY;
+    }
+
+
+}
+
