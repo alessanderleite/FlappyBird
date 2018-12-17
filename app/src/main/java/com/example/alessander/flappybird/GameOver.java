5735dbefc629f1a20614b3a627b1deac253b3514
alessanderleite <alessander.logistica@gmail.com>
2018-12-17 08:07:10 -0200

    create a new class

---
+++ app/src/main/java/com/example/alessander/flappybird/GameOver.java
@@ -0,0 +1,14 @@
+package com.example.alessander.flappybird;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class GameOver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_over);
+    }
+}
